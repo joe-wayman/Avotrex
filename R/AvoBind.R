270c451119b8025445fec0c14c71b334564c4bb8
Tom Matthews <txm676@gmail.com>
2023-10-09 09:31:27 +0000

    Testing access

--- R/AvoBind.R
+++ R/AvoBind.R
@@ -1,11 +1,4 @@
 #' AvoBind - Internal function 
-#' @param tree Tree object (i.e., phylogeny) 
-#' @param node Target node 
-#' @param per The fraction (0-1) of total branch length to truncate at either end of the branch for grafting (e.g. 0.2 cuts of 20% of the total branch lenth from either e
-#' @param per_fixed whether to graft on at an exact place rather than random; value between 0-1, with larger number meaning grafting happens closer to the root.
-#' @param sp_name Name of the grafted species
-#' @importFrom phytools bind.tip
-#' @export
 
 AvoBind <- function(
     tree, 
